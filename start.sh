48be20499161e1d7247ac2135278a4995f806ef8
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2024-01-12 03:07:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/MUB3 /LazyDeveloper
+git clone https://github.com/konichiwa55115/MUB4 /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
